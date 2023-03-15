f0743a9de5b5e7ffe4bf7e19da39d465d860cf5e
xabner233 <abner233.aso@gmail.com>
2023-03-15 09:08:05 -0300

    Criação Classe Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("descrição curso JS");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("mentoria java");
+        mentoria1.setDescricao("descrição mentoria java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+
+    }
+}
